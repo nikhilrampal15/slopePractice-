5dd2b689ed164c5dc87021ade22e50d64acf2ebf
Nikhil Rampal <nikhilrampal15@gmail.com>
2016-04-19 14:47:52 -0700

    slope excercise

---
+++ slope.js
@@ -0,0 +1,11 @@
+function slope(m){
+    if(m >= 0){
+        console.log("Things are trending up!");
+    }
+    else if(m===0){
+        console.log("undefined");
+    }
+    else{
+        console.log("things are trending down :(")
+    }
+}
